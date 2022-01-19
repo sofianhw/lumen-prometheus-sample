649cb98e08b7d8ab09ad880ad8ea5ec14c5d9fcc
Sofian Hadiwijaya <warungpintar@WARUNGs-MacBook-Air.local>
2022-01-19 16:58:38 +0700

    test controller

--- app/Http/Controllers/CollectorController.php
+++ app/Http/Controllers/CollectorController.php
@@ -22,7 +22,9 @@ class CollectorController extends Controller
      * @throws Exception
      */
     public function collect()
-        $histogram = $this->registerHistogram('test', 'some_histogram', 'it observes', ['type'], [0.1, 1, 2, 3.5, 4, 5, 6, 7, 8, 9]);
+    {
+        $registry = new Arquivei\LaravelPrometheusExporter\PrometheusExporter;
+        $histogram = $registry->registerHistogram('test', 'some_histogram', 'it observes', ['type'], [0.1, 1, 2, 3.5, 4, 5, 6, 7, 8, 9]);
         $histogram->observe($_GET['c'], ['blue']);
         echo "OK\n";
     }
