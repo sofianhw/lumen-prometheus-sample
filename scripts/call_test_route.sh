904cdfb3881a278bd8188097f37a4daabe629d06
Sofian Hadiwijaya <warungpintar@WARUNGs-MacBook-Air.local>
2022-01-19 22:23:43 +0700

    add sleep

--- scripts/call_test_route.sh
+++ scripts/call_test_route.sh
@@ -4,4 +4,5 @@ length=${TIMES_TO_CALL_EXAMPLE_ROUTE}
 for ((i=1;i<=$length;i++));
 do
    curl -v -XGET --header "Connection: keep-alive" "http://${EXAMPLE_APP_IP}:${EXAMPLE_APP_NGINX_PORT}/collector";
+   sleep 1
 done
